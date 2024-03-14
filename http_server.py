8c0f841c712a7a594e70fb8d43a4a08bad8355b8
waulkrie <aobare@gmail.com>
2024-03-13 19:44:20 -0500

    redirect, no render template

--- http_server.py
+++ http_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for, jsonify
+from flask import Flask, request, url_for, jsonify, render_template, redirect
 
 app = Flask(__name__)
 
@@ -10,15 +10,19 @@ def api():
     return jsonify(data)
 
 # POST request
-@app.route('/calculate', methods=['POST'])
-def calc():
-    data = request.get_json()
-    return jsonify(data)
+@app.route('/calculate')
+def calc(data = None):
+    if request.method == 'POST':
+        data = request.get_json()
+    else:
+        data = None
+    return render_template('templates/calculate.html', data=data)
+
 
 # GET request
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello World'
+    return redirect("/calculate")
 
 
 
