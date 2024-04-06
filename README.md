c2be5f2e9d7d8d4a76ddf1b09c0fb8ad189ff6ff
waulkrie <aobare@gmail.com>
2024-04-06 17:14:59 -0500

    working

---
+++ README.md
@@ -0,0 +1,6 @@
+# SysNet2 flask Http server
+
+Simple calculater with history
+
+pip install -r requirements.txt
+python3 http_server.py
--- http_server.py
+++ http_server.py
@@ -1,38 +1,61 @@
 from flask import Flask, request, url_for, jsonify, render_template, redirect
-import datetime
+from datetime import datetime, timezone
 
 app = Flask(__name__, template_folder='templates')
 
+# file handling
+def write_to_file(data):
+    with open('history.txt', 'a') as file:
+        file.write(data + '\n')
 
-# GET request
+
+def read_from_file():
+    with open('history.txt', 'r') as file:
+        return file.readlines()
+    
+#error catching
+@app.errorhandler(404)   
+def not_found(e): 
+  print("404 error NOT FOUND!")
+  return render_template("404.html") 
+
+
+# GET request for history api
 @app.route('/api/history', methods=['GET'])
 def api():
-    data = "History data from file here..."
+    print('Serving \'api/history\'')
+    data = read_from_file()
     return jsonify(data)
 
-# GET request
+
+# GET request to return calculation history
 @app.route('/history', methods=['GET'])
 def history():
-    data = request.get_json()
-    return render_template('/history.html', data=data)
+    history = read_from_file()
+    return render_template('/history.html', history=history)
 
-# POST request
+
+# POST and GET request for calculator
 @app.route('/calculate', methods=['GET', 'POST'])
 def calc(data = None):
     if request.method == 'POST':
-        history = request.form['expr']
-        print("HistoryP: ", history)
-        return redirect(url_for('calc'))
+        expr = request.form['expr']
+        try :
+            result = eval(expr) # executes valid python code, irl this is a vulnerability
+        except:
+            result = "Invalid expression"
+        write_to_file(expr + ' = ' + str(result))
+        print("Serving History Post: ", history)
+        return render_template('/calculate.html', expr=expr, result=result, utc_date=datetime.now(timezone.utc))
     else:
-        history = None
-        print("HistoryG: ", history)
-        return render_template('/calculate.html', history=history)
+        print('Serving \'calculate.html\'')
+        return render_template('/calculate.html', utc_date=datetime.now(timezone.utc))
 
 
 # GET request
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', utc_date=datetime.datetime.utcnow())
+    return redirect(url_for('calc'))
 
 
 
